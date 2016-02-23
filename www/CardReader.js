f9f71d85bed588ece9d3dc2e1c141a5d9cba93be
Rafał Kuć <qc.rafal@gmail.com>
2016-02-23 11:13:48 +0100

    improved js to communicate with native code

--- www/CardReader.js
+++ www/CardReader.js
@@ -1,18 +1,89 @@
 var exec = require('cordova/exec');
-function CardReader() { 
-	console.log("CardReader.js: is created");
+
+var clone = function clone(obj) {
+  if (null == obj || "object" != typeof obj) return obj;
+  var copy = obj.constructor();
+  for (var attr in obj) {
+    if (obj.hasOwnProperty(attr) && attr !== '_name') {
+      copy[attr] = obj[attr];
+    }
+  }
+  return copy;
+};
+
+
+function CardReader() {
+
+  var _self = this;
+  this.handlers = [];
+
+  console.log("CardReader.js: is created");
+
+  var success = function (data) {
+    for (var i = 0; i < _self.handlers.length; i++) {
+      if (data._name == _self.handlers[i].eventName) {
+        _self.handlers[i].handler(clone(data), null);
+      }
+    }
+  };
+
+  var fail = function (data) {
+    for (var i = 0; i < _self.handlers.length; i++) {
+      if (data._name == _self.handlers[i].eventName) {
+        _self.handlers[i].handler(null, clone(data));
+      }
+    }
+  };
+
+  exec(
+    success,
+    fail,
+    "CardReader",
+    "initializeCardReader",
+    []
+  );
 }
 
-CardReader.prototype.Read = function(success, fail){
-	console.log("CardReader Started");
-	exec(function(data){
-		console.log(data);
-		var result = data.split("/");
-		success(result[0], result[1], result[2]);
-	}, fail, "CardReader", "readCreditCard", []);
-	//exec(success, fail, "CardPlugin", "readCreditCard", []);
-}
+/*
+    Events: ready, read
+*/
 
-var cardReader = new CardReader(); 
+CardReader.prototype.on = function (eventName, handler) {
+  this.handlers.push({
+    eventName: eventName,
+    handler: handler
+  });
+};
+
+CardReader.prototype.ready = function (handler) {
+  this.on('ready', handler);
+};
+
+
+CardReader.prototype.isReady = function () {
+  exec(
+    function(ready){
+      console.log(ready);
+    },
+    function(){},
+    "CardReader",
+    "readIsReady",
+    []
+  );
+};
+
+CardReader.prototype.isConnected = function () {
+  exec(
+    function(ready){
+      console.log(ready);
+    },
+    function(){},
+    "CardReader",
+    "readIsConnected",
+    []
+  );
+};
+
+var cardReader = new CardReader();
 
 module.exports = cardReader;
